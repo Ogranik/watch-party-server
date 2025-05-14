20ba201e8d5dd2c90e61d8d6843cc1688ac140c6
Ogranik <57802312+Ogranik@users.noreply.github.com>
2025-05-14 22:41:45 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const socket = io("https://twoj-server.onrender.com"); // <- tu Twój backend
+
+const fileInput = document.getElementById("fileInput");
+const video = document.getElementById("videoPlayer");
+
+let isHost = false;
+
+fileInput.addEventListener("change", () => {
+  const file = fileInput.files[0];
+  if (file) {
+    const url = URL.createObjectURL(file);
+    video.src = url;
+    video.play();
+    isHost = true;
+  }
+});
+
+video.addEventListener("play", () => {
+  if (isHost) socket.emit("control", { action: "play", time: video.currentTime });
+});
+video.addEventListener("pause", () => {
+  if (isHost) socket.emit("control", { action: "pause", time: video.currentTime });
+});
+video.addEventListener("seeked", () => {
+  if (isHost) socket.emit("control", { action: "seek", time: video.currentTime });
+});
+
+socket.on("control", ({ action, time }) => {
+  if (!isHost) {
+    switch (action) {
+      case "play":
+        video.currentTime = time;
+        video.play();
+        break;
+      case "pause":
+        video.currentTime = time;
+        video.pause();
+        break;
+      case "seek":
+        video.currentTime = time;
+        break;
+    }
+  }
+});
